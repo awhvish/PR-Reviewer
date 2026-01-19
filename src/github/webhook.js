6751eac1a3fd5e422d13745c420a953b9ee5d739
Avis Lowha <high.avis.lowha@gmail.com>
2026-01-20 02:03:28 +0530

    test pr

--- src/github/webhook.js
+++ src/github/webhook.js
@@ -1,3 +1,17 @@
-export default (app) => {
-  app.log.info("GitHub Webhook Loaded");
-};
+export async function handlePullRequestEvent(context) {
+  const { owner, repo, number } = context.issue();
+  const pullRequest = context.payload.pull_request;
+
+  const prTitle = pullRequest.title;
+
+  context.log.info(
+    `Received pull request #${number} in ${owner}/${repo} with title: ${prTitle}`
+  );
+
+  return context.octokit.issues.createComment({
+    owner,
+    repo,
+    issue_number: number,
+    body: "Analyzing Pull Request. ",
+  });
+}
