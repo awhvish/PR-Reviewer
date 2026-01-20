e6f5e28e56f14fffba1789087db3451e497cc7ea
Avis Lowha <high.avis.lowha@gmail.com>
2026-01-20 17:37:39 +0530

    simple-git implementation

---
+++ src/git/cloner.ts
@@ -0,0 +1,42 @@
+import SimpleGit from "simple-git";
+import path from 'path';
+import fs from 'fs';
+
+export class RepoCloner {
+    private baseDir = "tmp/repositories/";
+
+    async cloneRepository(owner:string, repoName: string, token: string) {
+        const repoPath = path.join(this.baseDir, owner, repoName);
+
+        try {
+            if (await this.exists(repoPath)) {
+                console.log(`Repo ${repoName} exists, pulling updates...`)
+                const git = SimpleGit(repoPath);
+                await git.pull();
+                return;
+            }
+            console.log(`Cloning  ${owner}/${repoName}...`);
+            const git = SimpleGit();
+            const repoUrl = `https://x-access-token:${token}@github.com/${owner}/${repoName}.git`;
+            await git.clone(repoUrl, repoPath);
+
+            return repoPath;
+
+        }  catch (error) {
+            console.log(`ERROR: Failed to clone ${owner}/${repoName}: `, error)
+            throw error;
+        }
+    }
+    
+
+    private async exists(path: string): Promise<boolean> {
+        try {
+            await fs.promises.access(path, fs.constants.F_OK);
+            return true;
+        } catch(error) {
+            return false;
+        }
+    }
+}
+
+export const repoCloner = new RepoCloner();
